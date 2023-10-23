70fdc83d5b234bff194aa38c6ae97573650b1cce
adrianogomesz <adrianodede76@gmail.com>
2023-10-23 19:49:21 -0300

    creating views w/ handlebars engine

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express    = require('express');
 const app        = express();
+const exphbs     = require('express-handlebars');
+const path       = require('path');
 const db         = require('./db/connection');
 const bodyParser = require('body-parser');
 const router = require('./routes/jobs');
@@ -14,10 +16,21 @@ app.listen(PORT, function () {
 });
 
 
+// body parser
 app.use(bodyParser.urlencoded({ extended: false }));
 
 
-//db connection
+// handle bars
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+
+// static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// db connection
 db
     .authenticate()
     .then(() => { 
@@ -28,11 +41,10 @@ db
     });
 
 
-
 // routes
 app.get('/', (req, res) => {
 
-    res.send("Hello World!");
+    res.render('index');
 
 });
 
